8709e507ec583dd2dff73b5a458b425b84ffd705
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-12-17 17:55:59 +0700

    learnt

---
+++ lib/card_list_tile.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class CardListTile extends StatelessWidget {
+  const CardListTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Using Card List Tile',
+        ),
+      ),
+      body: Center(
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                listElement(),
+                listElement(),
+                listElement(),
+                listElement(),
+                listElement(),
+                listElement(),
+                listElement(),
+                listElement(),
+                listElement(),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('data'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SingleChildScrollView scrollView() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          listElement(),
+          listElement(),
+          listElement(),
+          listElement(),
+          listElement(),
+          listElement(),
+          listElement(),
+          listElement(),
+          listElement(),
+        ],
+      ),
+    );
+  }
+
+  Column listElement() {
+    return Column(
+      children: [
+        Card(
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              10,
+            ),
+          ),
+          child: const ListTile(
+            leading: FlutterLogo(),
+            title: Text('Title'),
+            subtitle: Text('Subtitle'),
+            trailing: Icon(Icons.accessibility_new_sharp),
+          ),
+        ),
+        const Divider(
+          indent: 5,
+          endIndent: 5,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/list_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UsingListView extends StatelessWidget {
+  const UsingListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Using List View'),
+      ),
+      body: Container(),
+    );
+  }
+}
